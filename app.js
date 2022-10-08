8f86960a566f8b93640def6336468ce39514534b
Tiago Covalesky <tcovalesky@imply.com>
2022-10-08 00:26:04 -0300

    feat: create app.js file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world from nodejs");
+});
+
+const port = 8080;
+
+app.listen(port);
