c3226fc9b49cf08c28b819b5cd81df8cb58f0335
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 20:27:30 -0300

    feat: adjusts get posts method

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,16 @@
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
-exports.getPosts = (req, res) =>
-  res.json({
-    posts: [{ title: "First post" }, { title: "Second post" }],
-  });
+exports.getPosts = (req, res) => {
+  const posts = Post.find()
+    .select("_id title body")
+    .then((posts) => res.json({ posts }))
+    .catch((err) => console.log(err));
+};
 
 exports.createPost = (req, res) => {
   const errors = validationResult(req);
   console.log(errors.errors);
   const post = new Post(req.body);
-  post.save().then((result) => res.status(201).json({ post: result }));
+  post.save().then((post) => res.status(201).json({ post }));
 };
