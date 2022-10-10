828dcb4b1e6a307d551a6e4b8e651e6ef9d9a9b7
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 20:42:38 -0300

    refactor: removes unused variables

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,14 @@
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
-  const posts = Post.find()
+  Post.find()
     .select("_id title body")
     .then((posts) => res.json({ posts }))
     .catch((err) => console.log(err));
 };
 
 exports.getPost = (req, res) => {
-  const post = Post.findById(req.params.id)
+  Post.findById(req.params.id)
     .select("_id title body")
     .then((post) => res.json({ post }))
     .catch((err) => console.log(err));
