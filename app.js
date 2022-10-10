1a77b52f840a66afe861c16b94c5b705ca7a3b7c
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 12:06:49 -0300

    feat: renames bodyParser instance and removes sample middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const bp = require("body-parser");
+const bodyParser = require("body-parser");
 const postRoutes = require("./routes/post");
 
 dotenv.config();
@@ -18,15 +18,9 @@ mongoose.connection.on("error", (err) => {
 const app = express();
 const port = process.env.PORT || 8080;
 
-const myMiddleware = (req, res, next) => {
-  console.log("sample middleware applied");
-  next();
-};
-
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(myMiddleware);
 
 app.use("/", postRoutes);
 
