121e499cd1b984f92c5d80c08faf3d33f42af752
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 12:11:14 -0300

    feat: removes length validation from mongoose schema

--- models/post.js
+++ models/post.js
@@ -4,14 +4,10 @@ const postSchema = new mongoose.Schema({
   title: {
     type: String,
     required: "Title is required",
-    minlength: 4,
-    maxlength: 150,
   },
   body: {
     type: String,
     required: "Body is required",
-    minlength: 4,
-    maxlength: 2000,
   },
 });
 
