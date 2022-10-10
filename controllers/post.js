50f06ce73f7882c4b47533156bdafc9c778fa1b7
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 10:34:47 -0300

    feat: adds controller method to create posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,16 @@
+const Post = require("../models/post");
+
 exports.getPosts = (req, res) =>
   res.json({
     posts: [{ title: "First post" }, { title: "Second post" }],
   });
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
+    res.status(201).json({ post: result });
+  });
+};
