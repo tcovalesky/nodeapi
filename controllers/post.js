7f0be52a8f939f119b30b3407659df4ace2807c1
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 20:39:43 -0300

    feat: create new method to get post by id

--- controllers/post.js
+++ controllers/post.js
@@ -1,10 +1,16 @@
-const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
   const posts = Post.find()
     .select("_id title body")
     .then((posts) => res.json({ posts }))
+    .catch((err) => console.log(err));
+};
+
+exports.getPost = (req, res) => {
+  const post = Post.findById(req.params.id)
+    .select("_id title body")
+    .then((post) => res.json({ post }))
     .catch((err) => console.log(err));
 };
 
