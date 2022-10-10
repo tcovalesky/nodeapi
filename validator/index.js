8d8a05d327676ef4c2060ad6fc1a5bc24e755c00
Tiago Covalesky <tcovalesky@imply.com>
2022-10-10 12:07:21 -0300

    feat: adds postvalidation rules and validation

---
+++ validator/index.js
@@ -0,0 +1,26 @@
+const { check, validationResult } = require("express-validator");
+
+exports.createPostValidationRules = [
+  check("title", "Write a title").notEmpty(),
+  check("title", "Title must be between 4 and 150 characters").isLength({
+    min: 4,
+    max: 150,
+  }),
+
+  check("body", "Write a body").notEmpty(),
+  check("body", "Body must be between 4 and 2000 characters").isLength({
+    min: 4,
+    max: 2000,
+  }),
+];
+
+exports.createPostValidator = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const firstError = errors.errors.map((error) => error.msg)[0];
+
+    return res.status(400).json({ error: errors.array() });
+  }
+
+  next();
+};
